296747d45f73e9126b22aa50cdcc740d3fcc164e
Onur Çelik <ourcelik@icloud.com>
2021-05-02 17:40:37 +0300

    DataAccess Added

---
+++ DataAccess/Concrete/TrendyolAppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class TrendyolAppDbContext : DbContext
+    {
+        public TrendyolAppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
