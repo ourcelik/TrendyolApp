ac14682b70a468e3c7e509f4cb3e15696d2e56f3
emreasik <asik.emre@outlook.com>
2021-05-14 21:46:50 +0300

    CategoryPage and ProductList ViewModels added

---
+++ TrendyolApp/TrendyolApp/ViewModels/CategoryPageViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using TrendyolApp.Data;
+using TrendyolApp.Models;
+using Xamarin.Forms;
+
+namespace TrendyolApp.ViewModels
+{
+    public class CategoryPageViewModel : BaseViewModel
+    {
+        public ObservableCollection<Category> Categories
+        {
+            get { return categories; }
+        }
+        public ObservableCollection<Category> categories;
+        public ObservableCollection<SubCategory> SubCategories
+        {
+            get { return subCategories; }
+            set
+            {
+                subCategories = value;
+                OnPropertyChanged(nameof(SubCategories));
+            }
+        }
+        public ObservableCollection<SubCategory> subCategories;
+        public ObservableCollection<SubSubCategory> SubSubCategories
+        {
+            get { return subSubCategories; }
+            set
+            {
+                subSubCategories = value;
+                OnPropertyChanged(nameof(SubSubCategories));
+            }
+        }
+        private ObservableCollection<SubSubCategory> subSubCategories;
+        public CategoryPageViewModel()
+        {
+            categories = new ObservableCollection<Category>(CategoryData.Categories);
+            subCategories = new ObservableCollection<SubCategory>();
+            subSubCategories = new ObservableCollection<SubSubCategory>();
+            OnPropertyChanged(nameof(SubSubCategories));
+            GetSubCategories = new Command((id) =>
+            {
+                subCategories.Clear();
+                SubCategoryData.SubCategories.Where(sc => sc.CategoryId == (int)id).ToList().ForEach((sc) => subCategories.Add(sc));
+                OnPropertyChanged(nameof(SubCategories));
+            });
+            GetSubSubCategories = new Command((id) =>
+            {
+                subSubCategories.Clear();
+                SubSubCategoryData.SubSubCategories.Where(sc => sc.SubCategoryId == (int)id).ToList().ForEach((sc) => subSubCategories.Add(sc));
+                OnPropertyChanged(nameof(SubCategories));
+            });
+        }
+        public Command GetSubCategories { get; }
+        public Command GetSubSubCategories { get; }
+    }
+}
---
+++ TrendyolApp/TrendyolApp/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using TrendyolApp.Data;
+using TrendyolApp.Models;
+
+namespace TrendyolApp.ViewModels
+{
+    class ProductListViewModel : BaseViewModel
+    {
+        ObservableCollection<ProductModel> listProducts;
+        public ObservableCollection<ProductModel> ListProducts
+        {
+            get
+            {
+                return listProducts;
+            }
+            set
+            {
+                listProducts = value;
+                OnPropertyChanged(nameof(ListProducts));
+            }
+
+        }
+
+        public ProductListViewModel(SubSubCategory category)
+        {
+            listProducts = new ObservableCollection<ProductModel>();
+            GetCategories(category);
+        }
+        public ProductListViewModel()
+        {
+
+        }
+        public ObservableCollection<ProductModel> GetCategories(SubSubCategory category)
+        {
+            List<ProductModel> data = ProductData.GetProducts().Where(p => p.Category.CategoryId == category.CategoryId
+            && p.SubCategory.SubCategoryId == category.SubCategoryId
+            && p.SubSubCategory.SubSubCategoryId == category.SubSubCategoryId).ToList();
+
+            data.ForEach(p => ListProducts.Add(p));
+            return ListProducts;
+
+        }
+    }
+}
